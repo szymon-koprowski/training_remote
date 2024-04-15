044724f403e56521864a8d784ac3e3f66a7cd104
Szymon Koprowski <szymon@deb-home.home>
2024-04-15 21:57:23 +0200

    dodanie funkcji obliczenia silni

--- projekt2.cpp
+++ projekt2.cpp
@@ -3,17 +3,24 @@
 using namespace std;
 
 // Projekt w wersji nr 2. Do pobrania z repozytorium zdalnego.
+int oblicz_silnie(int a){
+	int b = 1;
+	for (int i = 1; i <= a; i++){
+		b = b*i;
+	}
+	return b;
+}
 
 int main() {
-    int a, b;
-    cout << "Podaj liczbe calkowita ";
-    cin >> a >> b;
+    int a;
+    cout << "Podaj liczbe calkowita";
+    cin >> a;
     int wyjscie;
     do {
         cout << endl;
         cout << endl;
         cout << "MENU" << endl;
-        cout << "Podaj numer czynnosci, ktora chcesz wykonac" << endl;
+        cout << "Podaj numer czynnosci, ktora chcesz wykonac" << endl
         cout << "0. Wyjscie" << endl;
         cin >> wyjscie;
     } while(wyjscie != 0);
