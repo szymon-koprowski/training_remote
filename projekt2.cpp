1b20b375ff6721981a594cd6f41fc596cb60067d
szymon-koprowski <136858117+szymon-koprowski@users.noreply.github.com>
2024-04-15 20:44:04 +0200

    Create projekt2.cpp

---
+++ projekt2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+// Projekt w wersji nr 2. Do pobrania z repozytorium zdalnego.
+
+int main() {
+    int a, b;
+    cout << "Podaj liczbe calkowita ";
+    cin >> a >> b;
+    int wyjscie;
+    do {
+        cout << endl;
+        cout << endl;
+        cout << "MENU" << endl;
+        cout << "Podaj numer czynnosci, ktora chcesz wykonac" << endl;
+        cout << "0. Wyjscie" << endl;
+        cin >> wyjscie;
+    } while(wyjscie != 0);
+    return 0;
+}
