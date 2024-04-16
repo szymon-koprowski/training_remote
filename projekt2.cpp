63878aed4d2686ae8a7004a9f58e933a228ed80f
Szymon Koprowski <szymon@deb-home.home>
2024-04-16 19:40:48 +0200

    Dodanie funkcji sprawdzenia czy liczba jest pierwsza

--- projekt2.cpp
+++ projekt2.cpp
@@ -4,10 +4,18 @@ using namespace std;
 
 // Projekt w wersji nr 2. Do pobrania z repozytorium zdalnego.
 
+bool sprawdz_czy_pierwsza(int a){
+	int liczba_dzielnikow = 2;
+	for(int i = 2; i <= (a/2); i++){
+		if(a%i==0) liczba_dzielnikow++;
+	}
+	return liczba_dzielnikow==2;
+}
+
 int main() {
-    int a, b;
+    int a;
     cout << "Podaj liczbe calkowita ";
-    cin >> a >> b;
+    cin >>a;
     int wyjscie;
     do {
         cout << endl;
